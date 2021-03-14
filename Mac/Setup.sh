08ca31db182a29e677477807fa6b46cfcc94ff11
JaredChang <JaredChang@kkboxgroup.com>
2021-03-14 13:27:38 +0800

    build: update wrong dir in Mac Setup

--- Mac/Setup.sh
+++ Mac/Setup.sh
@@ -1,17 +1,17 @@
 working_directory=$(dirname "$0")
 
 rm -f ~/.gitalias
-ln $dir/../gitalias/gitalias.txt ~/.gitalias
+ln $working_directory/../gitalias/gitalias.txt ~/.gitalias
 rm -f ~/.gitconfig
-ln $dir/.gitconfig ~/.gitconfig
+ln $working_directory/.gitconfig ~/.gitconfig
 rm -f ~/.p10k.zsh
-ln $dir/.p10k.zsh ~/.p10k.zsh
+ln $working_directory/.p10k.zsh ~/.p10k.zsh
 rm -f ~/.zshrc
-ln $dir/.zshrc ~/.zshrc
+ln $working_directory/.zshrc ~/.zshrc
 mkdir ~/.ssh
 rm -f ~/.ssh/config
-ln $dir/ssh.config ~/.ssh/config
+ln $working_directory/ssh.config ~/.ssh/config
 
 softwareupdate --install-rosetta
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-sh $dir/homebrew.sh
+sh $working_directory/homebrew.sh
