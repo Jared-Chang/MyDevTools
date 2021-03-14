3cbb4c47bb6d6939977b682c0539ab00c6de1873
JaredChang <JaredChang@kkboxgroup.com>
2021-03-14 22:22:33 +0800

    build: homebrew env for register to PATH

--- Mac/homebrew.sh
+++ Mac/homebrew.sh
@@ -1,4 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 brew tap homebrew/cask
 
 brew install python3
