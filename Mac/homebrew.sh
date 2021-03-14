7e72ae654ae9e7c0490ff811b9a5740d5a7a8794
JaredChang <JaredChang@kkboxgroup.com>
2021-03-14 22:33:02 +0800

    build: update brew sh

--- Mac/homebrew.sh
+++ Mac/homebrew.sh
@@ -7,23 +7,24 @@ brew install pyenv
 pyenv install 3.9.1
 eval "$(pyenv init -)"
 pyenv global 3.9.1
+pip install --upgrade pip
 pip install cookiecutter
 
 brew install --cask jetbrains-toolbox
-brew install --cask pycharm             //backup settings
-brew install --cask visual-studio-code       //backup settings and plugin
-brew install --cask iterm2              //backup iterm2 settings
+brew install --cask pycharm             #backup settings
+brew install --cask visual-studio-code  #backup settings and plugin
+brew install --cask iterm2              #backup iterm2 settings
 brew install --cask docker
 
-brew install --cask bettertouchtool     //backup btt plugin
+brew install --cask bettertouchtool     #backup btt plugin
 brew install --cask sourcetrail
 brew install --cask appcleaner
 brew install --cask deepl
 brew install --cask 1password
-brew install --cask dash                //backup dash settings
+brew install --cask dash                #backup dash settings
 brew install --cask alfred              
 brew install --cask cheatsheet
-brew install --cask royal-tsx           //backup royal-tsx settings
+brew install --cask royal-tsx           #backup royal-tsx settings
 brew install --cask eul
 brew install --cask betterzip
 brew install --cask cleanshot
