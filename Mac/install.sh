9ecf54743ae2c3b6c94634d740a6cb1387cda593
JaredChang <JaredChang@kkboxgroup.com>
2021-03-15 11:33:47 +0800

    fix: non implement functino

--- Mac/install.sh
+++ Mac/install.sh
@@ -11,6 +11,16 @@ tty_red="$(tty_mkbold 31)"
 tty_bold="$(tty_mkbold 39)"
 tty_reset="$(tty_escape 0)"
 
+shell_join() {
+  local arg
+  printf "%s" "$1"
+  shift
+  for arg in "$@"; do
+    printf " "
+    printf "%s" "${arg// /\ }"
+  done
+}
+
 ohai() {
   printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
 }
