1c54f95179e8a12178b962b015f76a54d88a588d
JaredChang <JaredChang@kkboxgroup.com>
2022-03-22 13:05:57 +0800

    update zinit plugin

--- Mac/zsh/zinit.zsh
+++ Mac/zsh/zinit.zsh
@@ -11,12 +11,6 @@ source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
-zinit light-mode for \
-    zinit-zsh/z-a-rust \
-    zinit-zsh/z-a-as-monitor \
-    zinit-zsh/z-a-patch-dl \
-    zinit-zsh/z-a-bin-gem-node    
-
 ### End of Zinit's installer chunk
 
 zinit ice lucid wait='0' atinit='zpcompinit'
